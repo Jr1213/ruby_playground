d930a20bdc930a5d3593b51469cfe50e0384cf52
jr1213 <mohammed.jr.hammad@gmail.com>
2024-05-11 17:49:06 +0300

    init

---
+++ l1/test.rb
@@ -0,0 +1,10 @@
+puts "hi my name is mohammed"
+
+puts "     /|"
+puts "    / |"
+puts "   /  |"
+puts "  /   |"
+puts " /    |"
+puts "/_____|"
+
+puts "i'm 26 years old"
---
+++ l2/index.rb
@@ -0,0 +1,9 @@
+my_name = "ahmmed"
+speed_limit = "250"
+
+puts ("hi my name is " + my_name)
+my_name = "mohammed"
+puts (my_name + "loves cars")
+puts ("cars can move till " + speed_limit + " km/s")
+speed_limit = "400"
+puts (speed_limit + " km/s is very fast speed")
