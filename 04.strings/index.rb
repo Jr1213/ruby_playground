161b0d93ae520c08cb421f97724b9b68aeb6e90a
jr1213 <mohammed.jr.hammad@gmail.com>
2024-05-17 22:36:05 +0300

    finish string oprations

---
+++ 04.strings/index.rb
@@ -0,0 +1,17 @@
+skip_char = "        I Love \\Cars      "
+puts skip_char
+
+new_line = "l love \n cars"
+puts new_line
+
+
+puts skip_char.upcase()
+puts skip_char.downcase()
+puts skip_char.strip()
+puts (skip_char.strip().length())
+puts (skip_char.length())
+puts skip_char.include? "I"
+puts skip_char.include? "i"
+puts skip_char.strip()[2]
+puts skip_char.strip()[2,5]
+puts skip_char.strip().index("L")
